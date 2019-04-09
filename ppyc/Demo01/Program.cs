eb8991af5f7559427eb6dad08a8dd94ea392aae9
Emiliano Zacarias <emii.zacarias@gmail.com>
2019-04-09 16:26:25 -0300

    Demostracion manejo de Tareas

---
+++ ppyc/Demo01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    class Program
+    {
+        static void Metodo1()
+        {
+            Console.WriteLine("Se inicio metodo 1");
+        }
+        static void Metodo2()
+        {
+            Console.WriteLine("Se inicio metodo 2");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Factory.StartNew(Metodo1);
+
+            Task.WaitAll(t1);
+
+            Task t2 = new Task(Metodo2);
+            t2.Start();
+
+            Task.WaitAll(t2);
+
+            Console.WriteLine("Presione Enter para terminar le programa");
+            Console.Read();
+        }
+    }
+}
